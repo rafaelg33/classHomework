da66f8f47b10fac6d5d6265afe705366926dbeea
rafaelg33 <healthycornerrg120@gmail.com>
2017-08-29 22:00:49 -0400

    Add files via upload

---
+++ HW_70/index.php
@@ -0,0 +1,42 @@
+<?php
+
+
+if(!isset($_COOKIE['lastTimeVistited'])) {
+                 $TimeVistited = ' We are happy to get new users ';
+        } else {
+                 $TimeVistited = 'We are happy to have you back, you haven\'t been here since '. $_COOKIE['lastTimeVistited'];
+        }
+
+setcookie('lastTimeVistited', date('m/d/Y h:i:s A'), time()+3600);
+
+
+?>
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
+    <title>cookies</title>
+  </head>
+  <body>
+    <h1 class="text-center">Hello </h1>
+    <div class="alert alert-primary" role="alert">
+       <h2><?= $TimeVistited  ?></h2>
+  </div>
+
+
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js" integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/js/bootstrap.min.js" integrity="sha384-h0AbiXch4ZDo7tp9hKZ4TsHbi047NrKGLO3SEJAg45jXxnGIfYzk4Si90RDIqNm1" crossorigin="anonymous"></script>
+  </body>
+</html>
