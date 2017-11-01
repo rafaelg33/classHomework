dddb5bf538234563ddcb946f16ff7b9e9c1fcdc2
rafaelg33 <healthycornerrg120@gmail.com>
2017-10-31 21:23:17 -0400

    Homework 79

---
+++ HW_79/script.js
@@ -0,0 +1,20 @@
+
+document.getElementById("button").addEventListener("click", function(){
+
+    var Color = document.getElementById('color').value;
+    document.body.style.background = Color;
+
+});
+
+
+document.getElementById("buttontext").addEventListener("click", function(){
+    
+        var Color = document.getElementById('colortext').value;
+        document.body.style.color = Color;
+    
+    });
+
+
+ 
+
+
